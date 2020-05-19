36e115b23b45edd08677476044116d76526c920d
qianll <qll19930105@163.com>
2020-05-19 19:56:02 +0800

    reset jsonp

---
+++ src/api/recommend.js
@@ -0,0 +1,12 @@
+import jsconp from 'common/js/jsconp'
+import {commonParams, options} from './config'
+
+export function getRecommend() {
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+  return jsconp(url, data, options)
+}
