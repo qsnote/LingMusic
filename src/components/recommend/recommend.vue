7f4d7dd0d596ed063304395a5d13bab78c265912
qianll <qll19930105@163.com>
2020-05-25 21:04:26 +0800

    添加scroll控件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,37 +1,49 @@
 <template>
     <div class="recommend">
-        <div class="recommend-content">
-            <div v-if="recommends.length" class="slider-wrapper">
-              <slider>
-                <div v-for="(item,index) in recommends" :key="index">
-                  <a :href="item.linkUrl">
-                    <img :src="item.picUrl">
-                  </a>
-                </div>
-              </slider>
-            </div>
+      <scroll class="recommend-content" :data="discList">
+        <div>
+          <div v-if="recommends.length" class="slider-wrapper">
+            <slider>
+              <div v-for="(item,index) in recommends" :key="index">
+                <a :href="item.linkUrl">
+                  <img :src="item.picUrl">
+                </a>
+              </div>
+            </slider>
+          </div>
 
-        <div class="recommend-list">
+          <div class="recommend-list">
             <h1 class="list-title">热门歌单推荐</h1>
             <ul>
-                <li></li>
+                <li v-for="(item,index) in discList" :key="index" class="item">
+                  <div class="icon">
+                    <img :src="item.imgurl" width="60" height="60" alt="">
+                  </div>
+                  <div class="text">
+                    <h2 class="name" v-html="item.creator.name"></h2>
+                    <p class="desc" v-html="item.dissname"></p>
+                  </div>
+                </li>
             </ul>
+          </div>
         </div>
-        </div>
-
+      </scroll>
     </div>
 </template>
 <script>
 import Slider from 'base/slider/slider'
+import Scroll from 'base/scroll/scroll'
 import { getRecommend, getDiscList } from 'api/recommend'
 import {ERR_OK} from 'api/config'
 export default {
   components: {
-    Slider
+    Slider,
+    Scroll
   },
   data() {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   created() {
@@ -49,7 +61,7 @@ export default {
     _getDiscList() {
       getDiscList().then((res) => {
         if (res.code === ERR_OK) {
-          this.recommends = res.data.slider
+          this.discList = res.data.list
         }
       })
     }
@@ -57,25 +69,52 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-    @import '~common/stylus/variable'
+  @import '~common/stylus/variable'
 
-    .recommend
-        position fixed
+  .recommend
+    position fixed
+    width 100%
+    top 88px
+    bottom 0
+    .recommend-content
+      height 100%
+      overflow hidden
+      .slider-wrapper
+        position relative
         width 100%
-        top 88px
-        bottom 0
-        .recommend-content
-            height 100%
+        overflow hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display flex
+          box-sizing border-box
+          align-item center
+          padding 0 20px 20px
+          .icon
+            flex 0 0 60px
+            width 60px
+            padding-right 20px
+          .text
+            display flex
+            flex-direction column
+            justify-content center
+            flex 1
+            line-height 20px
             overflow hidden
-            .slider-wrapper
-                position relative
-                width 100%
-                overflow hidden
-            .recommend-list
-                .list-title
-                    height: 65px
-                    line-height: 65px
-                    text-align: center
-                    font-size: $font-size-medium
-                    color: $color-theme
+            font-size $font-size-medium
+            .name
+              margin-bottom 10px
+              color $color-text
+            .desc
+              color $color-text-d
+        .loading-container
+          position absolute
+          width 100%
+          top 50%
+          transform translateY(-50%)
 </style>
