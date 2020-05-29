82a02f0d0b5dad3a58571ccc075776e8e602366f
qianll <qll19930105@163.com>
2020-05-29 14:58:31 +0800

    增加singer页面

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,40 @@
 <template>
-    <div>
-        歌手页面
+    <div class="singer">
+        <list-view ref="list"></list-view>
     </div>
 </template>
 <script>
+import ListView from 'base/listview/listview'
+import { ERR_OK } from 'api/config'
+import { getSingerList } from 'api/singer'
 export default {
-
+  components: {
+    ListView
+  },
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(res.data.list)
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
-
+    .singer
+        position fixed
+        top 88px
+        bottom 0
+        width 100%
 </style>
