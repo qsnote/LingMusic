f329db88515effb86d8f01185bea9ab0f12ba833
qianll <qll19930105@163.com>
2020-06-05 10:05:14 +0800

    add singer-detail

---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/stylus/variable'
+.singer-detail
+  position fixed
+  z-index 100
+  top 0
+  left 0
+  right 0
+  bottom 0
+  background $color-background
+</style>
